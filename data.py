19ce57ca45246669732190f8114e5ed4107e9678
Djackree <djackree@yahoo.com>
2019-04-06 11:00:12 -0400

    The is the repo creation

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body':'Published on Dec 18, 2018. This Urdu story is about a fisherman and his friend who advise him to catch big fishes all the time. The fisherman accept his advise and every time he put his fish net in river he got small fish he waited for whole day to get big fish. The moral of the story is greedy is curse.',
+            'author':'Dinesh Jackree',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body':'Published on Dec 18, 2018. This Urdu story is about a fisherman and his friend who advise him to catch big fishes all the time. The fisherman accept his advise and every time he put his fish net in river he got small fish he waited for whole day to get big fish. The moral of the story is greedy is curse.',
+            'author':'Dinesh Yeet',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Two',
+            'body':'Published on Dec 18, 2018. This Urdu story is about a fisherman and his friend who advise him to catch big fishes all the time. The fisherman accept his advise and every time he put his fish net in river he got small fish he waited for whole day to get big fish. The moral of the story is greedy is curse.',
+            'author':'Yeet Jackree',
+            'create_date':'04-25-2017'
+        }
+    ]
+    return articles
